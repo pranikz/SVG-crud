9c347daf56668601e571a51dbbbf0a63a81bd3d4
pranikz <18btcse42@suiit.ac.in>
2023-03-05 21:32:51 +0530

    initial react and tailwind CSS setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-violet-900 text-7xl flex justify-center">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
